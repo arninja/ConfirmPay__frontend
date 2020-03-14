ccfec0d26cdaf1eb4703df4c93230ed164577952
Arnar Ingi Njarðarson <arnarnjardarson@gmail.com>
2020-03-14 19:28:47 +0000

    First commit by arninja

---
+++ app/src/main/java/com/example/confirmpay_frontend/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.confirmpay_frontend;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //Þegar við keyrum appið þá birtast ekki skilaboðin á opnunarskjánum (Hann verður tómur)
+        //Hér vísum við í hvaða síðu við viljum vitna í eftir keyrslu og skilaboð
+        //þeirrar síðu (Home) birtast.
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_Events:
+                            selectedFragment = new EventsFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
