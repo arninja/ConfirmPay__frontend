547d55b1d4b1116ce4624a35aea2223533d63ec8
Arnar Ingi Njarðarson <arnarnjardarson@gmail.com>
2020-03-15 15:37:07 +0000

    blala

--- app/src/main/java/com/example/confirmpay_frontend/EventsFragment.java
+++ app/src/main/java/com/example/confirmpay_frontend/EventsFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
-public class EventsFragment extends Fragment {
+public class EventsFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
